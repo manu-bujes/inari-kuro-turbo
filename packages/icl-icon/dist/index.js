115525c956bc2f2e97170e0fbf3a9aa186956de4
Manuel Bujes Garrido <manu.bujes@inari.io>
2023-05-05 12:19:16 +0200

    chore: add nx.json

--- packages/icl-icon/dist/index.js
+++ packages/icl-icon/dist/index.js
@@ -1,27 +1,14 @@
-(function() {
-  "use strict";
-  try {
-    if (typeof document != "undefined") {
-      var elementStyle = document.createElement("style");
-      elementStyle.appendChild(document.createTextNode(".icl-icon {\n  display: flex;\n  transform: scale(1);\n  stroke-width: 0;\n  stroke: currentColor;\n  fill: currentColor;\n}\n.icl-icon.default {\n  width: 24px;\n  height: 24px;\n}\n.icl-icon.small {\n  width: 18px;\n  height: 18px;\n}\n.icl-icon.medium {\n  width: 32px;\n  height: 32px;\n}\n.icl-icon.large {\n  width: 40px;\n  height: 40px;\n}\n.icl-icon.xlarge {\n  width: 60px;\n  height: 60px;\n}\n.icl-icon.big {\n  width: 96px;\n  height: 96px;\n}\n.icl-icon--is-primary {\n  color: var(--primary);\n}\n.icl-icon--is-secondary {\n  color: var(--secondary);\n}\n.icl-icon--is-grey {\n  color: var(--grey);\n}\n.icl-icon--is-info {\n  color: var(--info);\n}\n.icl-icon--is-success {\n  color: var(--success);\n}\n.icl-icon--is-warning {\n  color: var(--warning);\n}\n.icl-icon--is-error {\n  color: var(--error);\n}\n.icl-icon--is-clear {\n  color: var(--clear);\n}\n.icl-icon--is-white {\n  color: var(--white);\n}\n.icl-icon.disabled {\n  opacity: 0.4;\n  cursor: initial;\n}\n.icl-icon.cursor-pointer:not(.disabled) {\n  cursor: pointer;\n}\n.icl-icon.cursor-default {\n  cursor: default;\n}"));
-      document.head.appendChild(elementStyle);
-    }
-  } catch (e) {
-    console.error("vite-plugin-css-injected-by-js", e);
-  }
-})();
-import { openBlock, createElementBlock, normalizeClass, createBlock, resolveDynamicComponent } from "vue";
-const iclIcon_scss_vue_type_style_index_0_src_true_lang = "";
-const __default__ = {
+(function(){"use strict";try{if(typeof document<"u"){var i=document.createElement("style");i.appendChild(document.createTextNode(".icl-icon{display:flex;transform:scale(1);stroke-width:0;stroke:currentColor;fill:currentColor}.icl-icon.default{width:24px;height:24px}.icl-icon.small{width:18px;height:18px}.icl-icon.medium{width:32px;height:32px}.icl-icon.large{width:40px;height:40px}.icl-icon.xlarge{width:60px;height:60px}.icl-icon.big{width:96px;height:96px}.icl-icon--is-primary{color:var(--primary)}.icl-icon--is-secondary{color:var(--secondary)}.icl-icon--is-grey{color:var(--grey)}.icl-icon--is-info{color:var(--info)}.icl-icon--is-success{color:var(--success)}.icl-icon--is-warning{color:var(--warning)}.icl-icon--is-error{color:var(--error)}.icl-icon--is-clear{color:var(--clear)}.icl-icon--is-white{color:var(--white)}.icl-icon.disabled{opacity:.4;cursor:initial}.icl-icon.cursor-pointer:not(.disabled){cursor:pointer}.icl-icon.cursor-default{cursor:default}")),document.head.appendChild(i)}}catch(c){console.error("vite-plugin-css-injected-by-js",c)}})();
+import { openBlock as c, createElementBlock as n, normalizeClass as o, createBlock as r, resolveDynamicComponent as s } from "vue";
+const a = {
   name: "IclIcon"
-};
-const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+}, m = /* @__PURE__ */ Object.assign(a, {
   props: {
     /**
      * Icon component displayed..
      */
     icon: {
-      required: true,
+      required: !0,
       type: Object,
       default: () => {
       }
@@ -55,30 +42,25 @@ const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
      */
     disabled: {
       type: Boolean,
-      default: false
+      default: !1
     }
   },
   emits: ["click"],
-  setup(__props, { emit }) {
-    const props = __props;
-    const clickEvent = () => {
-      if (props.disabled)
-        return;
-      emit("click");
+  setup(e, { emit: t }) {
+    const l = e, i = () => {
+      l.disabled || t("click");
     };
-    return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("div", {
-        class: normalizeClass(["icl-icon", {
-          [`icl-icon--is-${__props.color} ${__props.size} cursor-${__props.cursor}`]: true,
-          disabled: __props.disabled
-        }]),
-        onClick: clickEvent
-      }, [
-        (openBlock(), createBlock(resolveDynamicComponent(__props.icon)))
-      ], 2);
-    };
+    return (u, d) => (c(), n("div", {
+      class: o(["icl-icon", {
+        [`icl-icon--is-${e.color} ${e.size} cursor-${e.cursor}`]: !0,
+        disabled: e.disabled
+      }]),
+      onClick: i
+    }, [
+      (c(), r(s(e.icon)))
+    ], 2));
   }
 });
 export {
-  _sfc_main as IclIcon
+  m as IclIcon
 };
--- tools/config/vite.config.base.js
+++ tools/config/vite.config.base.js
@@ -3,16 +3,12 @@ import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-export default defineConfig((params) => {
+export default defineConfig(() => {
   const pkg = process.env.npm_package_name;
   return {
     cacheDir: `../../node_modules/.vite/${pkg}`,
 
-    plugins: [
-      vue(),
-      eslint(),
-      cssInjectedByJsPlugin(),
-    ],
+    plugins: [vue(), eslint(), cssInjectedByJsPlugin()],
 
     // Uncomment this if you are using workers.
     // worker: {
@@ -26,7 +22,7 @@ export default defineConfig((params) => {
     // Configuration for building your library.
     // See: https://vitejs.dev/guide/build.html#library-mode
     build: {
-      minify: false,
+      minify: true,
       lib: {
         // Could also be a dictionary or array of multiple entry points.
         entry: 'src/index.js',
@@ -55,5 +51,5 @@ export default defineConfig((params) => {
       environment: 'jsdom',
       include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
-  }
+  };
 });
