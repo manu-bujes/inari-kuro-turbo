8dd32e1f5059cb080161de3d7a2a328841883c6f
Manuel Bujes Garrido <manu.bujes@inari.io>
2023-05-17 09:18:22 +0200

    chore(package): fix lerna scripts

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [
     (commit) => commit.toLowerCase().startsWith('wip'),
+    (commit) => commit.toLowerCase().startsWith('publish'),
   ],
 };
